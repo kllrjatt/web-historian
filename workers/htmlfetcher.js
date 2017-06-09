19e73cb7abac185445d5b5c12f40196b5727fe58
Gunpreet Singh <gunpreet@outlook.com>
2017-06-09 15:19:55 -0700

    add html fetcher

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,8 @@
 // Use the code in `archive-helpers.js` to actually download the urls
 // that are waiting.
+
+
+// add archive helper functions to worker 
+var archive = require('../helpers/archive-helpers');
+// read the listof urls and download Urls 
+archive.readListOfUrls(archive.downloadUrls);

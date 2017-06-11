0bfe723d0897955884ee2e39d2369d71c6cb01c2
Gunpreet Singh <gunpreet@outlook.com>
2017-06-10 18:56:52 -0700

    request handler in progress

--- web/request-handler.js
+++ web/request-handler.js
@@ -2,6 +2,34 @@ var path = require('path');
 var archive = require('../helpers/archive-helpers');
 // require more modules/folders here!
 
-exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+// use url to parse url 
+var url = require('url');
+// add http helpers 
+var helper = require('./http-helpers');
+
+// get or post methods 
+var requestAction = {
+  GET: function (request, response) {
+    // parse the url path and get pathname
+    var urlPath = url.parse(request.url).pathname;
+    // check to see what path is 
+    if (urlPath === '/') {
+      // replace path if no name 
+      urlPath = '/index.html';
+    }
+  },
+  POST: ''
+};
+
+exports.handleRequest = function (request, response) {
+  // check to see type of request 
+  var handleRequest = requestAction[request.method];
+  // there is a request 
+  if (handleRequest) {
+    //process a handle request 
+    handleRequest(request, response);
+  } else {
+    // throw error
+    helper.httpResponses.error(response);
+  }
 };
